5096e94753f53ec91b86c4ab9551286fc417a378
dingzebin <2530885279@qq.com>
2025-06-12 22:41:37 +0800

    2025-06-12学习结束

---
+++ 03-pizza-menu/parctice/src/index.js
@@ -0,0 +1,80 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import pizzaData from "./data";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="container">
+      <Header></Header>
+      <Menu></Menu>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+      <header className="header">
+        <h1>Fast React Pizza Co.</h1>
+      </header>
+  );
+  // const style = { color: "red", fontSize: "48px", textTransform: "uppercase" }
+  // return <h1 style={style}>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>0ur menu</h2>
+      {pizzaData.map((pizza) => {
+        return (
+          <Pizza
+            key={pizza.name}
+            name={pizza.name}
+            ingredients={pizza.ingredients}
+            price={pizza.price}
+            photoName={pizza.photoName}
+            soldOut={pizza.soldOut}
+          />
+        );
+      })}
+    </main>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString() +
+        "." +
+        (hour >= openHour && hour < closeHour
+          ? "We're currently open!"
+          : "We're closed!")}
+    </footer>
+  );
+  // return  React.createElement("footer", {}, "We're currently open!");
+}
+
+function Pizza({ name, ingredients, price, photoName, soldOut }) {
+  return (
+    <div>
+      <h3>{name}</h3>
+      <img src={photoName} alt={name} />
+      <p>{soldOut ? "Sold Out" : "Available"}</p>
+      <p>{ingredients}</p>
+      <p>price:{price}</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
