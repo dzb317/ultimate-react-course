35107c1d2800641e739434e62291703d80b1828d
dingzebin <2530885279@qq.com>
2025-08-10 23:18:38 +0800

    2025-08-10：学习完成

---
+++ 16-fast-react-pizza/parctice/src/ui/AppLayout.jsx
@@ -0,0 +1,24 @@
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
+
+function AppLayout() {
+    const navigation = useNavigation();
+    const isLoading = navigation.state === "loading";
+
+    return (
+        <div className="layout">
+            {isLoading && <Loader />}
+            <Header />
+            <div>
+                <main>
+                    <Outlet />
+                </main>
+            </div>
+            <CartOverview />
+        </div>
+    );
+}
+
+export default AppLayout;
---
+++ 16-fast-react-pizza/parctice/src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+
+function Header() {
+    return (
+        <header className="bg-yellow-400">
+            <Link to="/">Fast React Pizza Co.</Link>
+            <SearchOrder />
+        </header>
+    );
+}
+
+export default Header;
